080bc434ce1704bb6895e461ac8bc3453a8af09b
Diego Alves <diego.alves20@outlook.com>
2016-11-15 17:31:38 -0200

    Aula 3

--- lojaComEntity/Program.cs
+++ lojaComEntity/Program.cs
@@ -1,4 +1,5 @@
 ﻿using lojaComEntity.Entidades;
+using Microsoft.Data.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,40 @@ namespace lojaComEntity
     {
         static void Main(string[] args)
         {
-            UsuarioDao dao = new UsuarioDao();
-            Usuario diego = dao.BuscarPorId(1);
-            diego.Nome = "Diego Alves";
-            dao.SaveChanges();
+            EntidadesContext contexto = new EntidadesContext();
+
+            var categoria = contexto.Categorias.Include(c => c.Produtos).FirstOrDefault(c => c.ID == 1);
+
+            foreach(var p in categoria.Produtos)
+            {
+                Console.WriteLine(p.Nome);
+            }
+
+            //Produto p = contexto.Produtos.Include(produto => produto.Categoria).FirstOrDefault(produto => produto.CategoriaID == 1);
+            //Console.WriteLine(p.Nome);
+
+            //Categoria c = new Categoria()
+            //{
+            //    Nome = "informática"
+            //};
+
+            //contexto.Categorias.Add(c);
+            //contexto.SaveChanges();
+
+            //Produto p = new Produto()
+            //{
+            //    Nome = "Mouse",
+            //    Preco = 20,
+            //    Categoria = c
+            //};
+
+            //contexto.Produtos.Add(p);
+            //contexto.SaveChanges();
+
+            //UsuarioDao dao = new UsuarioDao();
+            //Usuario diego = dao.BuscarPorId(1);
+            //diego.Nome = "Diego Alves";
+            //dao.SaveChanges();
 
             //Usuario lucas = new Usuario()
             //{
